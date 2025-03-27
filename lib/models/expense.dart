cd8e474cd4a4fb3b0c6395d1c265b77639174798
Christopher Dorin <chrisdorin45@gmail.com>
2025-03-27 11:33:02 -0400

    created expenses list and refactored folders

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -15,5 +15,5 @@ class Expense{
   final String title;
   final double amount;
   final DateTime date;
-  final Category category; // leisure Expense(category : 'leisure') 
+  final Category category;
 }
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Cheeseburger',
+        amount: 11.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Movie Ticket',
+        amount: 18.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
